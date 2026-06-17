639b2d60b45d90d673a9e9c459fb092711ad08a9
Danniel Navas <dannielnavas@gmail.com>
2026-06-17 07:21:15 -0500

    feat: ✨ Add a transaction to record changes to the counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import { createSignal, type Component } from "solid-js";
+import { createEffect, createSignal, type Component } from "solid-js";
 
 const [count, setCount] = createSignal(0);
+
+createEffect(() => {
+  console.log("Count changed:", count());
+});
 const App: Component = () => {
   return (
     <p class="text-4xl text-green-700 text-center py-20">
